61a2e71582d11df3b0f39573ed416d3e2ce805da
justinrbeattie <justin.r.beattie@gmail.com>
2022-11-21 18:38:12 +1100

    postcss

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    'autoprefixer': {},
+    'postcss-import': {},
+    'postcss-preset-env': {
+      stage: 1,
+  },
+    'postcss-nested': {},
+    'css-has-pseudo': {},
+  },
+}
