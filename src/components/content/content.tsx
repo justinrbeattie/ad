6c9d4c360484aa4c908c2a98d8faa63891e4463b
justinrbeattie <justin.r.beattie@gmail.com>
2022-12-04 18:38:47 +1100

    content

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,10 +1,10 @@
-import { component$, Slot} from '@builder.io/qwik';
+import { component$} from '@builder.io/qwik';
 
 export default component$(() => {
 
   return (
     <div>
-      <Slot></Slot>
+  
     </div>
   );
 });
