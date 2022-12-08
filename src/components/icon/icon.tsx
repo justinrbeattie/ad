0d618333454d1a8b05930a9738fefe35b23c760c
justinrbeattie <justin.r.beattie@gmail.com>
2022-12-09 10:04:47 +1100

    string unicode

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -4,7 +4,7 @@ import styles from './icon.css?inline';
 export default component$((props:{ unicode: string, color: 'current'|'brand'| 'success'|'info'|'warning'|'danger', size: '16px'| '24px'| '32px'| '48px'|'64px', role: string, ariaLabel: string }) => {
   useStyles$(styles);
   return (
-    <i data-unicode={props.unicode} style={{'font-size': props.size, 'width':'calc(' + props.size + ' + 8px )', 'height':'calc(' + props.size + ' + 8px )', 'color': ' var(--' + props.color + ') '}}  role={props.role} arial-label={props.ariaLabel} >
+    <i data-unicode={'&#x' + props.unicode + ';'} style={{'font-size': props.size, 'width':'calc(' + props.size + ' + 8px )', 'height':'calc(' + props.size + ' + 8px )', 'color': ' var(--' + props.color + ') '}}  role={props.role} arial-label={props.ariaLabel} >
 
     </i>
   );
