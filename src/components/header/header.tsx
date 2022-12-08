9430b039cf40d3b799093872c7164faf8be7e2de
justinrbeattie <justin.r.beattie@gmail.com>
2022-12-08 15:54:00 +1100

    remove logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,4 @@
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
-import { QwikLogo } from '../icons/qwik';
 import styles from './header.css?inline';
 
 export default component$(() => {
@@ -8,10 +7,6 @@ export default component$(() => {
   return (
     <header>
       <div class="logo">
-        <a href="https://qwik.builder.io/" target="_blank">
-          <QwikLogo />
-
-        </a>
       </div>
       <ul>
         <li>
