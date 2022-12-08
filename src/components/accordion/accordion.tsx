3b1c58a7eca192dd147368e38debb8b8bc360f36
justinrbeattie <justin.r.beattie@gmail.com>
2022-12-08 20:37:21 +1100

    icon accordion

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -1,4 +1,5 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
+import Icon from '../icon/icon';
 import styles from './accordion.css?inline';
 
 export default component$((props: { heading: string}) => {
@@ -6,7 +7,7 @@ export default component$((props: { heading: string}) => {
   return (
     <details>
     <summary>
-      <div>{props.heading}</div>
+      <div>{props.heading}</div> <Icon unicode="&#xf105;" color="current" size="24px" role="image" ariaLabel="expand"/> 
     </summary>
     <div>
       <Slot></Slot>
