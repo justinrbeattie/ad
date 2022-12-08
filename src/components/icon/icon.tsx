c00221693791e6962fa951c72339a4f9a1ebfdfd
justinrbeattie <justin.r.beattie@gmail.com>
2022-12-09 10:21:38 +1100

    icon fix

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,14 +1,18 @@
-import { component$,useStyles$ } from '@builder.io/qwik';
+import { component$, useClientEffect$, useSignal, useStyles$ } from '@builder.io/qwik';
 import styles from './icon.css?inline';
 
 export default component$((props:{ unicode: string, color: 'current'|'brand'| 'success'|'info'|'warning'|'danger', size: '16px'| '24px'| '32px'| '48px'|'64px', role: string, ariaLabel: string }) => {
   useStyles$(styles);
+  const iconRef = useSignal<Element>();
+  useClientEffect$(() => {
+    if (iconRef.value) {
+      const iconElement = iconRef.value;
+      iconElement.innerHTML = props.unicode;
+    }
+  });
   return (
-    <i data-unicode={'&#x' + props.unicode + ';'} style={{'font-size': props.size, 'width':'calc(' + props.size + ' + 8px )', 'height':'calc(' + props.size + ' + 8px )', 'color': ' var(--' + props.color + ') '}}  role={props.role} arial-label={props.ariaLabel} >
+    <i style={{ 'font-size': props.size, 'width': 'calc(' + props.size + ' + 8px )', 'height': 'calc(' + props.size + ' + 8px )', 'color': ' var(--' + props.color + ') ' }}  role={props.role} arial-label={props.ariaLabel}  ref={iconRef}>
 
     </i>
   );
 });
-
-
-
