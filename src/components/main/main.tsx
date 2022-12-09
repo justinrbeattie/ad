6b240bc1544ffae12327920b076831c4140e14bc
justinrbeattie <justin.r.beattie@gmail.com>
2022-12-09 12:13:42 +1100

    editing class

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,9 +1,10 @@
 import { component$, Slot} from '@builder.io/qwik';
+import { useLocation } from '@builder.io/qwik-city';
 
 export default component$((props: {attributes: any}) => {
-
+  const location = useLocation();
   return (
-    <main    {...props.attributes} >
+    <main class={location.query.__builder_editing__ === 'true'? 'editing' : ''} {...props.attributes} >
       <Slot></Slot>
     </main>
   );
